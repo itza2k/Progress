a8f1cee2f41e2fc91af68be32006e99ca0dffa3a
itza2k <161753369+itza2k@users.noreply.github.com>
2024-11-26 20:36:02 +0530

    update tracker

--- app/src/main/java/com/itza2k/progress/MainActivity.kt
+++ app/src/main/java/com/itza2k/progress/MainActivity.kt
@@ -4,44 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.itza2k.progress.ui.theme.ProgressTheme
+import com.itza2k.progress.viewmodels.HealthTrackerViewModel
+import com.itza2k.progress.composables.HealthTracker
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: HealthTrackerViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ProgressTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HealthTracker(
+                        viewModel = viewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProgressTheme {
-        Greeting("Android")
     }
 }
